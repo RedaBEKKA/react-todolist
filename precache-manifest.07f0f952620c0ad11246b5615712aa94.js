44705d17d45a45d9718bf54df32ccc0fb55b6c85
unknown <reda.bekka@gmail.com>
2020-06-01 13:22:43 +0200

    Updates

---
+++ precache-manifest.07f0f952620c0ad11246b5615712aa94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b08177065b71ff231edb7db7d0f388",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "ab13b4aa22594499f857",
+    "url": "/react-todolist/static/css/main.4894c5f9.chunk.css"
+  },
+  {
+    "revision": "897e03f9b51531639ca2",
+    "url": "/react-todolist/static/js/2.23fe93c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.23fe93c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab13b4aa22594499f857",
+    "url": "/react-todolist/static/js/main.f0144e32.chunk.js"
+  },
+  {
+    "revision": "e93c7e7ec4275468d5ef",
+    "url": "/react-todolist/static/js/runtime-main.612d86ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
